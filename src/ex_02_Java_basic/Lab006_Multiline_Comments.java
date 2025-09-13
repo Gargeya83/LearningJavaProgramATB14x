d1570c161a03359ceac7351723782f154858f880
Gargeya83 <gbsbkb169@gmail.com>
2025-09-13 11:43:10 +0530

    13th sep 2025

--- src/ex_02_Java_basic/Lab006_Multiline_Comments.java
+++ src/ex_02_Java_basic/Lab006_Multiline_Comments.java
@@ -1,4 +1,27 @@
 package ex_02_Java_basic;
 
+/*
+ * Author : Gargeya
+ * Course :  ATB14x
+ * Learning : Java
+ * This is a documentation comment
+ */
+
 public class Lab006_Multiline_Comments {
-}
+        /*
+         * Author : Gargeya
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         */
+
+public static void main(String[] args) {
+        /*
+        This is multiline comment
+        This is second line
+        This is third line
+         */
+            System.out.println("This is multiline comment");
+        }
+    }
+
