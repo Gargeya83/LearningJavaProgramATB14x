e147a7ec6109083c87b6ef5ec3f66e63a9936da6
Gargeya83 <gbsbkb169@gmail.com>
2025-09-13 12:00:00 +0530

    Update ex_02_Java_basic: add/fix examples (Lab003, Lab007, Lab013, Lab015, Lab021)

--- src/ex_02_Java_basic/Lab007_Keywords_Identifiers.java
+++ src/ex_02_Java_basic/Lab007_Keywords_Identifiers.java
@@ -1,5 +1,13 @@
 package ex_02_Java_basic;
 
+// Keywords are reserved words in Java that have a predefined meaning and cannot be used for naming variables,
+// methods, classes, or other identifiers.
+
+// Identifiers, on the other hand, are names that you create to identify variables, methods, classes, and other entities in your code.
+// Identifiers must follow certain rules,such as starting with a letter, underscore (_), or dollar sign ($), and cannot be the same as keywords.
+// In the provided code, `package`, `public`, `class`, and `static` are keywords, while
+// `Lab007_Keywords_Identifier` and `main` are identifiers.
+
 public class Lab007_Keywords_Identifiers {
     public static void main(String[] args) {
         System.out.println("This is keywords and identifiers");
--- src/ex_02_Java_basic/Lab015_Variables.java
+++ src/ex_02_Java_basic/Lab015_Variables.java
@@ -8,12 +8,14 @@ public class Lab015_Variables {
         //```
 
         byte age = 65;
-       byte age_sunil = 45;
-//        short age_sunil = 65;
+        byte age_sunil = 45;
+        // short age_sunil = 65;
+
         // byte -> data type - type of container
-        //  age_sunil ->  variable_name |  identifier
+        // age_sunil ->  variable_name |  identifier
         // = -> Assignment Operator ?  (after some time)
         // 65 -> Variable Value |  Literal
+
         byte age2 = 30;
         age = 66;
         age_sunil = 122;
@@ -21,5 +23,19 @@ public class Lab015_Variables {
         System.out.println(age_sunil);
         System.out.println(age);
         System.out.println(age2);
+
+        /* data_type variable_name or identifier = variable_value or literal */
+
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;   // Floating point number
+        char myLetter = 'D';        // Character
+        boolean myBool = true;      // Boolean
+        String myText = "Hello";    // String
+
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Character: " + myLetter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
     }
 }
