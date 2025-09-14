08404509089bdeacbb06b96b1b10500599455aaa
Gargeya83 <gbsbkb169@gmail.com>
2025-09-14 14:39:33 +0530

    13th sep 2025

--- src/ex_02_Java_basic/Lab021_Boolean.java
+++ src/ex_02_Java_basic/Lab021_Boolean.java
@@ -3,7 +3,7 @@ package ex_02_Java_basic;
 public class Lab021_Boolean {
     public static void main(String[] args) {
         boolean is_Java_Fun = true;
-        boolean isFishTasty=false;
+        boolean isFishTasty = false;
         boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
         //There's no set length which can be said to be the ideal length for a variable name
         System.out.println(is_Java_Fun);
