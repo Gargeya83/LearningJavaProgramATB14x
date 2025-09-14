3818363276fd93ade2f0c98e07e69d1238d23e02
Gargeya83 <gbsbkb169@gmail.com>
2025-09-14 14:00:17 +0530

    14th sep 2025

--- src/ex_02_Java_basic/Table_of_9_with_printf.java
+++ src/ex_02_Java_basic/Table_of_9_with_printf.java
@@ -2,38 +2,18 @@ package ex_02_Java_basic;
 
 public class Table_of_9_with_printf {
     public static void main(String[] args) {
-        int table = 9;
-
-      /*  System.out.printf("%d*1=%d%n", a, a*1);
-        System.out.printf("%d*2=%d%n", a, a*2);
-        System.out.printf("%d*3=%d%n", a, a*3);
-        System.out.printf("%d*4=%d%n", a, a*4);
-        System.out.printf("%d*5=%d%n", a, a*5);
-        System.out.printf("%d*6=%d%n", a, a*6);
-        System.out.printf("%d*7=%d%n", a, a*7);
-        System.out.printf("%d*8=%d%n", a, a*8);
-        System.out.printf("%d*9=%d%n", a, a*9);
-       */// System.out.printf("%d*10=%d%n", a, a*10);
+        int a = 9;
 
         System.out.println("Below displays Multiplication Table of 9 :");
-        System.out.printf("%d * 1= %d " , table, table);
-        System.out.println();
-        System.out.printf("%d * 2= %d"  , table, table*2);
-        System.out.println();
-        System.out.printf("%d * 3= %d"  , table, table*3);
-        System.out.println();
-        System.out.printf("%d * 4= %d"  , table, table*4);
-        System.out.println();
-        System.out.printf("%d * 5= %d"  , table, table*5);
-        System.out.println();
-        System.out.printf("%d * 6= %d"  , table, table*6);
-        System.out.println();
-        System.out.printf("%d * 7= %d"  , table, table*7);
-        System.out.println();
-        System.out.printf("%d * 8= %d"  , table, table*8);
-        System.out.println();
-        System.out.printf("%d * 9= %d"  , table, table*9);
-        System.out.println();
-        System.out.printf("%d * 10= %d"  , table, table*10);
+        System.out.printf("%d * 1 = %d%n", a, a*1);
+        System.out.printf("%d * 2 = %d%n", a, a*2);
+        System.out.printf("%d *3 = %d%n", a, a*3);
+        System.out.printf("%d * 4 = %d%n", a, a*4);
+        System.out.printf("%d * 5 = %d%n", a, a*5);
+        System.out.printf("%d * 6 = %d%n", a, a*6);
+        System.out.printf("%d * 7 = %d%n", a, a*7);
+        System.out.printf("%d * 8 = %d%n", a, a*8);
+        System.out.printf("%d * 9 = %d%n", a, a*9);
+       System.out.printf("%d * 10 = %d%n", a, a*10);
     }
 }
