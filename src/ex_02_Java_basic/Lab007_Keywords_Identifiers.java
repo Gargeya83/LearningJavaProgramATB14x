87d4cd088c4f05e0d0d245b58c9cdd4c132531fb
Gargeya83 <gbsbkb169@gmail.com>
2025-09-21 18:16:59 +0530

    13th sep 2025

--- src/ex_02_Java_basic/Lab007_Keywords_Identifiers.java
+++ src/ex_02_Java_basic/Lab007_Keywords_Identifiers.java
@@ -8,10 +8,10 @@ package ex_02_Java_basic;
 * - Identifiers must follow certain rules,such as starting with a letter, underscore (_), or dollar sign ($), and cannot be the same as keywords.
 * - In the provided code, `package`, `public`, `class`, and `static` are keywords, while
 * - `Lab007_Keywords_Identifier` and `main` are identifiers.
-* - * - **Simple English words** which are having predefined meaning in **Java Programming Language.**
-* - * - Keywords are also called **Reserved Words.**
-* - * - All the keywords are defined in **Lower Case.  **
-* - * - We **can’t use keywords** as names for variables, methods, classes, or as any other identifiers.
+* - Simple English words** which are having predefined meaning in **Java Programming Language.**
+* - Keywords are also called **Reserved Words.**
+* - All the keywords are defined in **Lower Case.  **
+* - We **can’t use keywords** as names for variables, methods, classes, or as any other identifiers.
  */
 
 public class Lab007_Keywords_Identifiers {
