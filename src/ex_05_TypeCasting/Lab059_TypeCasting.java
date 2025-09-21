86a8681cb61db105e4dcc5709fc19319135d88d1
Gargeya83 <gbsbkb169@gmail.com>
2025-09-21 21:58:09 +0530

    20th sep 2025

---
+++ src/ex_05_TypeCasting/Lab059_TypeCasting.java
@@ -0,0 +1,13 @@
+package ex_05_TypeCasting;
+
+public class Lab059_TypeCasting {
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = b; // Valid Syntax -> Widening -> Implicit Casting - Automatically done.
+        System.out.println(a);
+        int a1 = (int) b; // Valid Syntax -> Widening -> Explicit Casting - not required
+        // Explicit casting where we tell the JVM to datatype
+        System.out.println(b);
+
+    }
+}
---
+++ src/ex_06_Ternary_Operator/Lab063_Ternary_Operator.java
@@ -0,0 +1,9 @@
+package ex_06_Ternary_Operator;
+
+public class Lab063_Ternary_Operator {
+    public static void main(String[] args) {
+        int age =18;
+        String canMeetiJhaGoGoa =  age > 18 ? "Yes" : "No";
+        System.out.println(canMeetiJhaGoGoa);
+    }
+}
