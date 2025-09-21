be0e12c0eb2173447a98645a76bee4d1a27639e9
Gargeya83 <gbsbkb169@gmail.com>
2025-09-21 18:16:15 +0530

    13th sep 2025

--- src/ex_02_Java_basic/Lab007_Keywords_Identifiers.java
+++ src/ex_02_Java_basic/Lab007_Keywords_Identifiers.java
@@ -2,17 +2,18 @@ package ex_02_Java_basic;
 
 /*
 * #### Keywords
-// Keywords are reserved words in Java that have a predefined meaning and cannot be used for naming variables,
-// methods, classes, or other identifiers.
-// Identifiers, on the other hand, are names that you create to identify variables, methods, classes, and other entities in your code.
-// Identifiers must follow certain rules,such as starting with a letter, underscore (_), or dollar sign ($), and cannot be the same as keywords.
-// In the provided code, `package`, `public`, `class`, and `static` are keywords, while
-// `Lab007_Keywords_Identifier` and `main` are identifiers.
-// * - **Simple English words** which are having predefined meaning in **Java Programming Language.**
-// * - Keywords are also called **Reserved Words.**
-// * - All the keywords are defined in **Lower Case.  **
-// * - We **can’t use keywords** as names for variables, methods, classes, or as any other identifiers.
+* - Keywords are reserved words in Java that have a predefined meaning and cannot be used for naming variables,
+* - methods, classes, or other identifiers.
+* - Identifiers, on the other hand, are names that you create to identify variables, methods, classes, and other entities in your code.
+* - Identifiers must follow certain rules,such as starting with a letter, underscore (_), or dollar sign ($), and cannot be the same as keywords.
+* - In the provided code, `package`, `public`, `class`, and `static` are keywords, while
+* - `Lab007_Keywords_Identifier` and `main` are identifiers.
+* - * - **Simple English words** which are having predefined meaning in **Java Programming Language.**
+* - * - Keywords are also called **Reserved Words.**
+* - * - All the keywords are defined in **Lower Case.  **
+* - * - We **can’t use keywords** as names for variables, methods, classes, or as any other identifiers.
  */
+
 public class Lab007_Keywords_Identifiers {
     public static void main(String[] args) {
         System.out.println("This is keywords and identifiers");
