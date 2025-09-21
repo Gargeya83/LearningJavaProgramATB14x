8c41881c3457a171361c41151a82405c74ddf630
Gargeya83 <gbsbkb169@gmail.com>
2025-09-21 20:51:59 +0530

    14th sep 2025

--- src/ex_04_Operators/Lab042_Logical_Operators.java
+++ src/ex_04_Operators/Lab042_Logical_Operators.java
@@ -4,14 +4,12 @@ public class Lab042_Logical_Operators {
     public static void main(String[] args) {
        /*
         boolean a = true;
-
         System.out.println(!a);
 
         boolean b = true;
         System.out.println(!!b);
 
         boolean c= true || false;
-
         System.out.println(c);
 
         boolean c1= false && true;
