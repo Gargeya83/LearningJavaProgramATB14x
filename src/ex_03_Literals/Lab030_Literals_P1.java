04a5a48aeff56dcd8e46cc5cff8193c7c8abfbda
Gargeya83 <gbsbkb169@gmail.com>
2025-09-21 20:31:07 +0530

    13th sep 2025
    This reverts commit d7eb45c1

--- src/ex_03_Literals/Lab030_Literals_P1.java
+++ src/ex_03_Literals/Lab030_Literals_P1.java
@@ -2,9 +2,14 @@ package ex_03_Literals;
 
 public class Lab030_Literals_P1 {
     public static void main(String[] args) {
-        int age = 65;  // literal - Integral
+        // Integral literal
+        int age = 65;
+        int Enum = 9; //This is the valid variable because E is capital
+        System.out.println("These are the integral literal = " + age + ", " + Enum);
         System.out.println(age);
-        final float pi = 3.14f;   // literal - float
+
+        // Float literal
+        final float pi = 3.14f;
         System.out.println(pi);
         final int girl_age = 18;
         //girl_age = 19;
