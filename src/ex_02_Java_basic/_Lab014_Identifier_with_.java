12ed9ac14b649660f17745327df69939e1d3d09f
Gargeya83 <gbsbkb169@gmail.com>
2025-09-21 20:34:40 +0530

    13th sep 2025
    This reverts commit 04a5a48aeff56dcd8e46cc5cff8193c7c8abfbda.

--- src/ex_02_Java_basic/_Lab014_Identifier_with_.java
+++ src/ex_02_Java_basic/_Lab014_Identifier_with_.java
@@ -1,18 +1,9 @@
 package ex_02_Java_basic;
 
-    /*
- Rules to follow when you define an Identifier:**
-    1. Identifier can contain Alphabets, Digits, and two special symbol i.e. Dollar ($), _,.
-    2. Underscore (_).
-    3. First character of an identifier must be an Alphabet(a-z,A-Z) or Dollar ($) or Underscore (_).
-    4. Keywords or Reserved words can't be used as Identifiers.
-    5. name convention - first letter capital and other letter capital also - CamelCase
- */
-
-    public class _Lab014_Identifier_with_ {
-        public static void main(String[] args) {
-            //Identifier with _ (underscore)
-            //int _ = 10;
-            //System.out.println(_);
-        }
+public class _Lab014_Identifier_with_ {
+    //Identifier with _ (underscore)
+    public static void main(String[] args) {
+        //int _ = 10;
+        //System.out.println(_);
     }
+}
--- src/ex_03_Literals/Lab030_Literals_P1.java
+++ src/ex_03_Literals/Lab030_Literals_P1.java
@@ -2,14 +2,9 @@ package ex_03_Literals;
 
 public class Lab030_Literals_P1 {
     public static void main(String[] args) {
-        // Integral literal
-        int age = 65;
-        int Enum = 9; //This is the valid variable because E is capital
-        System.out.println("These are the integral literal = " + age + ", " + Enum);
+        int age = 65;  // literal - Integral
         System.out.println(age);
-
-        // Float literal
-        final float pi = 3.14f;
+        final float pi = 3.14f;   // literal - float
         System.out.println(pi);
         final int girl_age = 18;
         //girl_age = 19;
