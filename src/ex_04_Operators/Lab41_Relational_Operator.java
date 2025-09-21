9dc20982e690706b78f91a6a50a276b342136cc1
Gargeya83 <gbsbkb169@gmail.com>
2025-09-21 20:54:38 +0530

    14th sep 2025

--- src/ex_04_Operators/Lab41_Relational_Operator.java
+++ src/ex_04_Operators/Lab41_Relational_Operator.java
@@ -1,5 +1,13 @@
 package ex_04_Operators;
 
+/* Relational Operators -> Used to compare two values
+/* 1. < (Less than)
+   2. <= (Less than or equal to)
+   3. > (Greater than)
+   4. >= (Greater than or equal to)
+   5. = (Equal to)
+   6. != (Not equal to)
+ */
 public class Lab41_Relational_Operator {
     public static void main(String[] args) {
         int a = 10;
