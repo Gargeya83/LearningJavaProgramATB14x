76f844ceef097b45e629e0e8120ff867309ab5c6
Gargeya83 <gbsbkb169@gmail.com>
2025-09-28 11:07:48 +0530

    13th sep 2025

--- src/ex_02_Java_basic/Lab016_Variables.java
+++ src/ex_02_Java_basic/Lab016_Variables.java
@@ -2,6 +2,8 @@ package ex_02_Java_basic;
 
 public class Lab016_Variables {
     public static void main(String[] args) {
+
+        // we will be able to do arithmetic operations on the data in variables, we can print them directly by giving variable name in print statement
         int a=10;
         a= 65 + 1;
         System.out.println(a);
--- src/ex_02_Java_basic/Lab017_Variables.java
+++ src/ex_02_Java_basic/Lab017_Variables.java
@@ -2,7 +2,7 @@ package ex_02_Java_basic;
 
 public class Lab017_Variables {
     public static void main(String[] args) {
-        // byte age = 128; // Out of range.
+        // byte age = 128; // Out of range.or out of size range of byte It supports from -128 to 127
         int age = 128;
         age = age+1;
         age = 76;
