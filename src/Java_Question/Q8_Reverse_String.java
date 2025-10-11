17b8430dc1d0f25b1f0fd060bfad267c621f4dfb
Gargeya83 <gbsbkb169@gmail.com>
2025-10-11 13:34:25 +0530

    05th oct 2025

--- src/Java_Question/Q8_Reverse_String.java
+++ src/Java_Question/Q8_Reverse_String.java
@@ -19,5 +19,93 @@ public class Q8_Reverse_String {
     }
 }
 
+  /*
+ Approach 1
+		/*
+		String s = "Welcome";
+	    String rev = "";
+
+		for(int i=s.length()-1; i>=0;i--)
+		{
+			System.out.print(s.charAt(i));
+		}
+
+------------------------------------------------------------------------
+		// Approach 2
+
+		String s = "Welcome";
+	    String rev = "";
+
+		for(int i=s.length()-1; i>=0;i--)
+		{
+		rev= rev+s.charAt(i);
+		}
+			System.out.print("Reverse string i:"+rev);
+
+-------------------------------------------------------------
+
+		// Approach 3: - Without using string method
+		String s= "Welcome";
+
+		String rev="";
+
+		char a[]=s.toCharArray();// this will convert string to character type array
+		for (int i=a.length-1;i>=0;i--)
+		{
+			System.out.println(a[i]);
+		}
+
+
+-----------------------------------------------------------------------------
+		// Approach 4:By converting string char array type
+				//by converting string into array
+
+        String s= "Welcome";	//1.Original string
+        String rev = "";        // 2. An empty string 'rev' is initialized to store the reversed string.
+        char a[] = s.toCharArray();  // 3. Convert the original string into a character array.
+
+        System.out.print("Character Array:");
+        for (int i=0; i<a.length; i++) {
+        System.out.print(a[i] + " ");
+        }
+
+		for (int i=a.length-1;i>=0;i--)
+		{
+			rev= rev+a[i];
+			System.out.println("Reverse string:" +rev);
+			}
+
+---------------------------------------------------------------
+
+		// Approach  5: Using string buffer class
+
+
+		StringBuffer s=new StringBuffer("Welcome");
+
+		System.out.println("Reverse string:"+ s.reverse());
+
+--------------------------------------------------------------------
+Approach 6: Using string builder class
+
+StringBuilder s=new StringBuilder("Welcome");
+
+System.out.println("Reverse string:" +s.reverse());
+
+------------------------------------------------------------------
+Using While loop
+
+  Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter the string you want to reverse:");
+        String stringBuilder= scanner.next();
+        int len=stringBuilder.length();
+        String revers="";
+        while(len>0){
+            revers=revers+stringBuilder.charAt(len-1);
+            len--;
+        }
+        System.out.println("reverse is :"+revers);
+    }
+}
+   */
 
 
--- src/Java_Question/Q9_Count_number_of_words_string.java
+++ src/Java_Question/Q9_Count_number_of_words_string.java
@@ -34,3 +34,51 @@ public class Q9_Count_number_of_words_string {
 }
 
 
+/*
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter String");
+    String str = scanner.nextLine();
+    int count = 0;
+    boolean inWord = false;
+    for (int i = 0 ; i < str.length() ; i++) {
+        char ch = str.charAt(i);
+        if (ch != ' ' && !inWord) {
+            inWord = true;
+            count++;
+        } else if (ch == ' ') {
+            inWord = false;
+        }
+    }
+    System.out.println ("No. of words: " + count);
+}
+}
+
+------------------------------------------------------------------------
+public class NumberOfWords {
+    public static void main(String[] args) {
+        String str="Here is your string to validate the number of words.";
+        String [] splitStr=str.split(" ");
+        int len=splitStr.length;
+        System.out.println("total number of words in string is: "+len);
+        for (int i = 0; i < len; i++) {
+            System.out.println("first letter of each word is:"+splitStr[i].charAt(0));
+        }
+
+    }
+}
+
+-------------------------------------------------------------------
+public class P4_NumberOfWordsString {
+    public static void main(String[] args) {
+
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter The String I will count the words");
+    String userInput= sc.nextLine();
+    String[] S1= userInput.split(" ");
+    int count =0;
+     for(String s:S1) {
+          count++;
+     } System.out.println(count);
+}}
+ */
