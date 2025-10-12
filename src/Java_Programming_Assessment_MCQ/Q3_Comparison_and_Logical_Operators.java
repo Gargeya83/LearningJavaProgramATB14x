e0838199da8b432835693956e332d5f67c9cda42
Gargeya83 <gbsbkb169@gmail.com>
2025-10-12 12:10:08 +0530

    12th oct 2025

--- src/Java_Programming_Assessment_MCQ/Q3_Comparison_and_Logical_Operators.java
+++ src/Java_Programming_Assessment_MCQ/Q3_Comparison_and_Logical_Operators.java
@@ -21,12 +21,9 @@ public class Q3_Comparison_and_Logical_Operators {
         System.out.println("a >= b : " + (a >= b));
 
         // Logical operators
-        boolean a1 = true;
-        boolean b1 = false;
-
-        System.out.println("\na1 && b1 (AND) : " + (a1 && b1));
-        System.out.println("a1 || b1 (OR)  : " + (a1 || b1));
-        System.out.println("!b1 (NOT)      : " + (!b1));
+        System.out.println("(a > b) && (a > 0): " + ((a > b) && (a > 0)));
+        System.out.println("(a < b) || (a > 0): " + ((a < b) || (a > 0)));
+        System.out.println("!(a > b): " + (!(a > b)));
 
     }
 }
