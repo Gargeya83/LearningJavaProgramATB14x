358b91e8e8ef188121a37e1dfc786d031d9f3100
Gargeya83 <gbsbkb169@gmail.com>
2025-10-12 11:43:56 +0530

    12th oct 2025

--- src/Java_Programming_Assessment_MCQ/Q3_Comparison_and_Logical_Operators.java
+++ src/Java_Programming_Assessment_MCQ/Q3_Comparison_and_Logical_Operators.java
@@ -10,22 +10,23 @@ package Java_Programming_Assessment_MCQ;
  */
 public class Q3_Comparison_and_Logical_Operators {
     public static void main(String[] args) {
-        int a = 10, b=20;
+        int a = 10, b = 20;
 
-        System.out.println(a==b);
-        System.out.println(a!=b);
-        System.out.println(a<b);
-        System.out.println(a>b);
-        System.out.println(a<=b);
-        System.out.println(a>=b);
+        // Comparison operators
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a <= b : " + (a <= b));
+        System.out.println("a >= b : " + (a >= b));
 
+        // Logical operators
         boolean a1 = true;
         boolean b1 = false;
-        System.out.println(a1 && b1);
-        System.out.println(a1 || b1);
-        System.out.println(!b1);
 
-
+        System.out.println("\na1 && b1 (AND) : " + (a1 && b1));
+        System.out.println("a1 || b1 (OR)  : " + (a1 || b1));
+        System.out.println("!b1 (NOT)      : " + (!b1));
 
     }
 }
