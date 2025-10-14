ecb45aa027fec8393ba2381e1143eb1aa75496eb
Gargeya83 <gbsbkb169@gmail.com>
2025-10-14 22:20:09 +0530

    11th oct 2025

---
+++ src/ex_17_OOPs/Lab172_P2.java
@@ -0,0 +1,18 @@
+package ex_17_OOPs;
+
+public class Lab172_P2 {
+    public static <Baby> void main(String[] args) {
+        ATBx amit = new ATBx();
+        // Class - ATBx
+        // Object Ref - amit
+        // Object -> new ATBx();
+
+        amit.name = "Amit sharma";
+        amit.doAssignment();
+
+        Baby b1 = new Baby();
+        Baby b2 = new Baby();
+        Baby b3 = new Baby();
+    }
+}
+
