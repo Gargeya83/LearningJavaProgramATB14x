231710c62248c5016d29f5eabf8d5eb466a86205
Gargeya83 <gbsbkb169@gmail.com>
2025-10-26 20:14:39 +0530

    17th oct 2025

--- src/Java_Question/Q20_Smallest_Of_2_Numbers.java
+++ src/Java_Question/Q20_Smallest_Of_2_Numbers.java
@@ -16,3 +16,56 @@ public class Q20_Smallest_Of_2_Numbers {
         System.out.println("Smallest among 2 numbers is "+smallest);
     }
 }
+/*
+public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the 2 numbers");
+        int a,b;
+        if(sc.hasNextInt()) {
+             a = sc.nextInt();
+        }
+        else {
+            System.out.println("Invalid input.Enter a valid first number");
+            return;
+        }
+        if(sc.hasNextInt()) {
+             b = sc.nextInt();
+        }
+        else {
+            System.out.println("Invalid input.Enter a valid second number");
+            return;
+        }
+        if(a< b)
+        {
+            System.out.println("Smallest number is " +a);
+        }
+        else if(a==b)
+        {
+            System.out.println("Two numbers are equal.Enter different numbers");
+
+        }
+        else {
+            System.out.println("Smallest number is " +b);
+        }
+    }
+}
+ */
+
+/*
+ public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the 2 numbers");
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
+        if(num1<num2){
+            System.out.println("num1 is smaller"+num1);
+        }
+        else if(num2<num1){
+            System.out.println("num2 is smaller"+num2);
+        }
+        else{
+            System.out.println("Both numbers are equal");
+        }
+    }
+}
+ */
