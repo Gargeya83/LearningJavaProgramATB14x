c73f055e4299c55e924fb951968e1d9040280dc2
Gargeya83 <gbsbkb169@gmail.com>
2025-10-26 19:34:31 +0530

    05th oct 2025

--- src/Java_Question/Q9_Count_number_of_words_string.java
+++ src/Java_Question/Q9_Count_number_of_words_string.java
@@ -164,3 +164,33 @@ public class Q004_Number_Of_Words_InString {
 }
  */
 
+/*
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your text");
+        String sr = scanner.nextLine();
+        String[] count = sr.split(" ");
+        System.out.println(count.length);
+        // Count number of words
+        System.out.println(sr);
+        System.out.println("Total count of words are " + count.length);
+       // System.out.println(count[0]);
+
+        // first alphabet of each word in string
+
+        // First way
+        for (int i=0 ; i<count.length;  i++){
+            System.out.print(count[i].charAt(0) + " ");
+        }
+        System.out.println("");
+        // 2nd way
+
+        for (String firstletter : count){
+            System.out.println(firstletter.toUpperCase().charAt(0));
+        }
+
+
+        //}
+    }
+}
+ */
