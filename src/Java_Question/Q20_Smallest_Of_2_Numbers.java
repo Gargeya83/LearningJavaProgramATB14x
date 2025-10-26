16f3568a837323363141174071cafaae85ce902c
Gargeya83 <gbsbkb169@gmail.com>
2025-10-26 20:09:17 +0530

    17th oct 2025

---
+++ src/Java_Question/Q20_Smallest_Of_2_Numbers.java
@@ -0,0 +1,18 @@
+package Java_Question;
+import java.util.Scanner;
+
+//Find the Smallest of Two Numbers.
+public class Q20_Smallest_Of_2_Numbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st number ");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter 2nd number ");
+        int n2 = scanner.nextInt();
+
+        int smallest= (n1<n2)? n1 :n2;
+
+        System.out.println("Smallest among 2 numbers is "+smallest);
+    }
+}
