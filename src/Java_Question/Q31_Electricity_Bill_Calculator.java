0330771d678ec8fadb6bc0e0aa2fa9f7e1a9db68
Gargeya83 <gbsbkb169@gmail.com>
2025-10-26 21:56:25 +0530

    23rd oct 2025

---
+++ src/Java_Question/Q31_Electricity_Bill_Calculator.java
@@ -0,0 +1,36 @@
+package Java_Question;
+/*
+Electricity Bill Calculation (Based on Units Consumed)
+           :- take the input from the user of Units.
+Implement Rate Structure:
+Define the rate structure for calculating the bill based on the number of units consumed.
+        :-  First 100 units: 0.50Rs per unit
+            :-  Next 100 units (101-200): 0.75Rs per unit
+            :- Next 100 units (201-300): 1.20Rs per unit
+            :- Above 300 units: 1.50Rs per unit
+            */
+
+import java.util.Scanner;
+
+public class Q31_Electricity_Bill_Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Units Consumed: ");
+        int units = sc.nextInt();
+
+        double bill;
+
+        if (units <= 100)
+            bill = units * 0.50;
+        else if (units <= 200)
+            bill = (100 * 0.50) + (units - 100) * 0.75;
+        else if (units <= 300)
+            bill = (100 * 0.50) + (100 * 0.75) + (units - 200) * 1.20;
+        else
+            bill = (100 * 0.50) + (100 * 0.75) + (100 * 1.20) + (units - 300) * 1.50;
+
+        System.out.println("Total Electricity Bill: ₹" + bill);
+    }
+}
+
