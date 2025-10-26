825f3f637252b470ba0328cb3318a08cb67d5971
Gargeya83 <gbsbkb169@gmail.com>
2025-10-26 21:47:25 +0530

    23rd oct 2025

---
+++ src/Java_Question/Q29_Number_is_an_Armstrong_Number.java
@@ -0,0 +1,24 @@
+package Java_Question;
+import java.util.Scanner;
+public class Q29_Number_is_an_Armstrong_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum = sum + (digit * digit * digit);
+            num = num / 10;
+        }
+
+        if (original == sum)
+            System.out.println(original + " is an Armstrong number.");
+        else
+            System.out.println(original + " is NOT an Armstrong number.");
+    }
+}
+
