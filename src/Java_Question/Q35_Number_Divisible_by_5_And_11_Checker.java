777724db75d2af37c6cbb733a0dfd13f6b2de732
Gargeya83 <gbsbkb169@gmail.com>
2025-10-29 09:48:57 +0530

    24th oct 2025

---
+++ src/Java_Question/Q35_Number_Divisible_by_5_And_11_Checker.java
@@ -0,0 +1,20 @@
+package Java_Question;
+import java.util.Scanner;
+// Check if a number is divisible by 5 and 11
+public class Q35_Number_Divisible_by_5_And_11_Checker {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+
+        int num=sc.nextInt();
+        if(num%5==0 && num%11==0)
+        {
+            System.out.println("Number is divisible by both 5 and 11");
+        }
+        else{
+            System.out.println("Not divisible by 5 and 11");
+        }
+    }
+}
+
