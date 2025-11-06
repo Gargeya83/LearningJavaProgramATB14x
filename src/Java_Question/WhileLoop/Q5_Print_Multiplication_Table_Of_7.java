597b951dddbcb5724969c99e9a310a21518cbe4e
Gargeya83 <gbsbkb169@gmail.com>
2025-11-06 10:10:15 +0530

    26th oct 2025

---
+++ src/Java_Question/WhileLoop/Q5_Print_Multiplication_Table_Of_7.java
@@ -0,0 +1,13 @@
+package Java_Question.WhileLoop;
+
+public class Q5_Print_Multiplication_Table_Of_7 {
+
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=10)
+        {
+            System.out.println("7 * "+n+" = "+ 7*n);
+            n++;
+        }
+    }
+}
